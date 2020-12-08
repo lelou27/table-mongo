a9752e7c99d30dc384e09bc79dc5668f86466da5
Guegan <leandre.guegan@linkt.fr>
2020-12-08 16:28:03 +0100

    Add connection to mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://root:root@cluster0.6hyks.mongodb.net/table-mongo',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
